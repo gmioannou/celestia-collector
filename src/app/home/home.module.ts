b485b9cbcb0d7aaf70c12d929492e222596a90da
gmioannou <gmioannou@gmail.com>
2019-11-30 23:12:14 +0200

    Initial Commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { IonBottomDrawerModule } from 'ion-bottom-drawer';
 
 import { HomePage } from './home.page';
 
@@ -11,6 +12,8 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    IonBottomDrawerModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
