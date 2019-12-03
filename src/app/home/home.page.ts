2f4ea79d922d927f1ea28db786201c828b3cec84
gmioannou <gmioannou@gmail.com>
2019-12-03 05:13:52 +0200

    Add splash sceen and icon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IonBottomDrawerModule, DrawerState } from 'ion-bottom-drawer';
-import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
-import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Plugins, CameraResultType, CameraSource, SplashScreen } from '@capacitor/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { MapViewService } from '../services/map-view.service';
 import { Platform } from '@ionic/angular';
@@ -14,7 +14,7 @@ import { Platform } from '@ionic/angular';
 export class HomePage implements OnInit {
   @ViewChild("map", { static: false }) mapEl: ElementRef;
 
-  drawerState = 0;
+  drawerState = DrawerState.Bottom;
   dockedHeight = 400;
 
   photoList: any[] = []
@@ -29,26 +29,34 @@ export class HomePage implements OnInit {
   subscribe: any;
 
   constructor(
-    private formBuilder: FormBuilder, 
-    private sanitizer: DomSanitizer, 
-    private mapViewService: MapViewService, 
-    private platform: Platform) 
-  {
-    this.subscribe = this.platform.backButton.subscribeWithPriority(66666, () => {
-      if (this.constructor.name == "MapViewerPage") {
-        if (window.confirm("Are you sure you want to quit Cyclops Collector?")){
-          navigator["app"].exitApp();
-        }
-      }  
-    })
+    private formBuilder: FormBuilder,
+    private sanitizer: DomSanitizer,
+    private mapViewService: MapViewService,
+    private platform: Platform) { }
+
+  ionViewDidEnter() {
+    this.subscribe = this.platform.backButton.subscribe(() => {
+      if (window.confirm("Are you sure you want to quit?")){
+        navigator["app"].exitApp();
+      }
+    });
+  }
+
+  ionViewWillLeave() {
+    this.subscribe.unsubscribe();
   }
 
   async ngOnInit() {
 
+    // show splash screen
+    SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true
+    });
+
     // get mapview
     try {
       this.loading = true
-      this.mapViewService.delay(20000)
 
       let res = await this.mapViewService.getMapView()
       this.mapView = res
@@ -57,17 +65,20 @@ export class HomePage implements OnInit {
 
       let laydesc = await this.mapViewService.getFetureLayerDescriptor()
       let renderer = laydesc.data.drawingInfo.renderer
-      
+
       this.featureLayerField = renderer.field1
       this.featureLayerDomain = renderer.uniqueValueInfos
 
       this.featureLayerDomain.forEach(element => {
-          element.isSelected = false
+        element.isSelected = false
       });
+
+      let copyrightElement = document.getElementsByClassName("esri-attribution__powered-by")
+      copyrightElement[0].innerHTML = "© 2019 Cyprus University of Technology"
 
       // select the first element of the list
       this.selectFeatureType(this.featureLayerDomain[0])
-      
+
       console.log('Layer descriptor initialized...');
 
     }
@@ -79,7 +90,7 @@ export class HomePage implements OnInit {
     }
   }
 
-  // form for capturing new events
+  // define events form
   eventForm: FormGroup = this.formBuilder.group({
     hazard_type: new FormControl(""),
     description: new FormControl(""),
@@ -93,13 +104,12 @@ export class HomePage implements OnInit {
   selectFeatureType(featureType) {
     this.eventForm.controls['hazard_type'].setValue(featureType.value);
     this.featureLayerDomain.forEach(element => {
-      if (element == featureType){
+      if (element == featureType) {
         element.isSelected = true
         this.featureTypeSelected = element
       }
       else element.isSelected = false
     });
-
   }
 
   // capture new event
@@ -109,13 +119,13 @@ export class HomePage implements OnInit {
       this.loading = true
 
       await this.mapViewService.captureEvent(this.eventForm, this.photoList)
-      
+
       // reset event form and list of photos
       this.eventForm.reset()
       this.photoList = []
       this.photoListPreview = []
 
-      this.toggleDrawerState()
+      this.hideDrawerState()
 
     }
     catch (e) {
@@ -127,7 +137,7 @@ export class HomePage implements OnInit {
 
   }
 
-  // take photo for attachement
+  // take photos and place them in the attachments list
   async takePhoto() {
     const image = await Plugins.Camera.getPhoto({
       quality: 80,
@@ -140,17 +150,21 @@ export class HomePage implements OnInit {
     this.photoListPreview.push(this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl)));
   }
 
+  // remove photo from the attachments list
   removePhoto(idx) {
     this.photoList.splice(idx, 1);
     this.photoListPreview.splice(idx, 1);
   }
 
-  // show/hide drawer (bottom sheet)
-  toggleDrawerState() {
-    if (this.drawerState == DrawerState.Bottom)
-      this.drawerState = DrawerState.Docked
-    else
-      this.drawerState = DrawerState.Bottom
+  // show drawer (bottom sheet)
+  showDrawerState() {
+    this.drawerState = DrawerState.Docked
   }
+
+  // hide drawer (bottom sheet)
+  hideDrawerState() {
+    this.drawerState = DrawerState.Bottom
+  }
+
 
 }
